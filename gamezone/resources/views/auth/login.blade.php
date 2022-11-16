5576119236c42f409d56d8099cdd5f8a641df75a
Bazielich_Tomasz <karoshiplay@gmail.com>
2022-11-16 20:34:34 +0100

    v 0.5.3 Zmiana wygladu formularza logowania

--- gamezone/resources/views/auth/login.blade.php
+++ gamezone/resources/views/auth/login.blade.php
@@ -1,22 +1,96 @@
 @extends('layouts.app')
 
 @section('content')
+<link href="../assets/css/login-style.css" rel="stylesheet" />
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+  <link rel='stylesheet' href='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/themes/smoothness/jquery-ui.css'><link rel="stylesheet" href="./style.css">
 
-                <div class="card-body">
+<body>
+<!-- partial:index.partial.html -->
+
+<div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
+<div class='login'>
+  <div class='login_title'>
+  <img src="{{URL::asset('img/logo3.png')}}">
+  </div>
+  <div class='login_fields'>
+    <div class='login_fields__user'>
+      <div class='icon'>
+        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/user_icon_copy.png'>
+      </div>
+      <input placeholder='Adres E-mail' type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+       
+      <div class='validation'>
+          <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+        </div>
+      </input>
+    </div>
+    <div class='login_fields__password'>
+      <div class='icon'>
+        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/lock_icon_copy.png'>
+      </div>
+      <input placeholder='Haslo' id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+      <div class='validation'>
+        <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/tick.png'>
+      </div>
+    </div>
+     
+    <div></div>
+    <div>
+        <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+
+<label class="form-check-label"  id="pamietaj" for="remember">
+    Zapamiętaj mnie
+</label>      
+</div>
+    <div class='login_fields__submit'>
+      <input type='submit' value='Zaloguj'>
+      <div class='forgot'>
+    
+ 
+
+
+        @if (Route::has('password.request'))
+                                    <a class="btn btn-link" href="{{ route('password.request') }}">
+                                        Zapomniales hasla?
+                                    </a>
+                                @endif
+                                
+                  
+        </div>
+        
+     
+
+
+    </div>
+  </div>
+  <div class='success'>
+    <h2>Authentication Success</h2>
+    <p>Welcome back</p>
+  </div>
+  <div class='disclaimer'>
+    <!-- <p>Nie posiadasz konta? Zarejestruj się</p>                TUTAJ CHCIALEM PODLINKOWAC REJESTRACJE ALE NIE WIEM JAK             -->
+  </div>
+</div>
+    <div class='authent'>
+  <img src='https://s3-us-west-2.amazonaws.com/s.cdpn.io/217233/puff.svg'>
+  <p>Authenticating...</p>
+    </div>
+
+    <!-- partial -->
+  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+    <script src='//ajax.googleapis.com/ajax/libs/jqueryui/1.11.2/jquery-ui.min.js'></script><script  src="./script.js"></script>
+
+    </body>
+
                         <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>
-
                             <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
                                 @error('email')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -24,13 +98,8 @@
                                 @enderror
                             </div>
                         </div>
-
                         <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>
-
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
@@ -39,35 +108,10 @@
                             </div>
                         </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
             </div>
         </div>
     </div>
-</div>
+        </form>
+    </div>
+    </div>
 @endsection
--- gamezone/resources/views/layouts/app.blade.php
+++ gamezone/resources/views/layouts/app.blade.php
@@ -22,13 +22,14 @@
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+            <!--     PRZYCISK LARAVEL
+            <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'Laravel') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-
+-->
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <!-- Left Side Of Navbar -->
                     <ul class="navbar-nav me-auto">
@@ -38,6 +39,9 @@
                     <!-- Right Side Of Navbar -->
                     <ul class="navbar-nav ms-auto">
                         <!-- Authentication Links -->
+
+
+                        <!-- 
                         @guest
                             @if (Route::has('login'))
                                 <li class="nav-item">
@@ -69,6 +73,7 @@
                                 </div>
                             </li>
                         @endguest
+-->
                     </ul>
                 </div>
             </div>
